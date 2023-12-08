e6bc208014190231659fcc6e0fe44e86c7882bc9
e71828 <740943170@qq.com>
2023-12-08 14:01:23 +0800

    five

--- TrackingUsingDistributedSynchronousPassiveSensorsExample.m
+++ TrackingUsingDistributedSynchronousPassiveSensorsExample.m
@@ -205,34 +205,6 @@ assignmentTable(:,{'TrackID','AssignedTruthID','TotalLength','FalseTrackStatus'}
 % The estimated error for each truth is higher. Notice that the track
 % jumps in the theater display above.
 disp(cumulativeTruthMetrics(errorMetrics));
-%% Track with Five Sensors
-% In this section, detections from all five sensors are used for tracking
-% and a measurement noise of 2 degrees squared is used.
-[detectionBuffer,truthLog,theaterDisplay] = helperGenerateStaticFusionScenarioData;
-% keyboard;
-showScenario(theaterDisplay); 
-measNoise = 2; % Same noise as 3 sensors
-numSensors = 3;
-[trackingMetrics,errorMetrics] = helperRunStaticFusionSimulation(detectionBuffer,truthLog,numSensors,measNoise,theaterDisplay,true);
-axes(theaterDisplay.TheaterPlot.Parent);
-%%
-% ylim([0 1.5]);
-
-%%
-% The assignment results from tracking using five sensors show that all
-% truths were assigned a track during the entire simulation. There were
-% also no track drops in the simulation as compared to 4 track drops in the
-% low-accuracy three sensor simulation.
-assignmentTable = trackMetricsTable(trackingMetrics);
-assignmentTable(:,{'TrackID','AssignedTruthID','TotalLength','FalseTrackStatus'})
-
-%%
-% The estimated errors for positions are much lower for each true target as
-% compared to the three sensor simulation. Notice that the estimation
-% results for position and velocity do degrade as compared to three sensors
-% with high-accuracy measurements.
-disp(cumulativeTruthMetrics(errorMetrics))
-
 %% Summary
 % This example showed how to track objects using a network of distributed
 % passive sensors. You learned how to use |staticDetectionFuser| to
